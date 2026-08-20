e9dff23e849dbaeddb99ec5f99011194a39b1002
Ryan Hausen <ryan.hausen@gmail.com>
2026-08-20 15:26:17 -0400

    added A100 job

---
+++ A100_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=a100_job
+#SBATCH --partition=a100
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=2
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --time=00:05:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=rhausen@jhu.edu
+
+make all
